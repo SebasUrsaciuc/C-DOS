3e51b9062807bb020ce0bb017858f8cd8395c773
FoxWareFX <foxwarefx@gmail.com>
2021-12-22 17:18:04 +0200

    transformed ps2 functions in defines / refactoring

--- include/ps2.h
+++ include/ps2.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#define PS2CTRL_DATAPORT 0x60
-#define PS2CTRL_SYSPORT 0x61
-#define PS2CTRL_COMPORT 0x64
+#include "port.h"
 
-#define PS2CTRL_COM_RESET 0xFE
+#define PS2_CTRL_DATAPORT 0x60
+#define PS2_CTRL_SYSPORT 0x61
+#define PS2_CTRL_CMDPORT 0x64
+
+#define PS2_CMD_RESET 0xFE
 
 /* Reboots the system. */
-void reset();
+#define ps2_reset() port_outb(PS2_CTRL_CMDPORT, PS2_CMD_RESET);
