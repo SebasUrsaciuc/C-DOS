a8ee2194317de4e451e92a4bd1cc51fd75f39c04
FoxWareFX <foxwarefx@gmail.com>
2021-12-22 17:12:42 +0200

    added sect & asmv defines, int typedefs

--- include/def.h
+++ include/def.h
@@ -1,22 +1,22 @@
 #pragma once
 
-#define null ((void*) 0)
+#define NULL ((void*) 0)
 
 #define attr(...) __attribute__((__VA_ARGS__))
+#define sect(s) __section__("." #s)
+#define asmv(...) asm volatile(__VA_ARGS__)
 
-typedef __INT8_TYPE__ int8;
-typedef __UINT8_TYPE__ uint8;
-typedef __INT16_TYPE__ int16;
-typedef __UINT16_TYPE__ uint16;
-typedef __INT32_TYPE__ int32;
-typedef __UINT32_TYPE__ uint32;
+#define PACKED attr(packed)
+
+typedef __INT8_TYPE__ i8, int8, int8_t;
+typedef __UINT8_TYPE__ u8, uint8, uint8_t, byte;
+typedef __INT16_TYPE__ i16, int16, int16_t;
+typedef __UINT16_TYPE__ u16, uint16, uint16_t, word;
+typedef __INT32_TYPE__ i32, int32, int32_t;
+typedef __UINT32_TYPE__ u32, uint32, uint32_t, dword;
 
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
-
-typedef uint8 byte;
-typedef uint16 word;
-typedef uint32 dword;
 
 typedef enum { false, true } bool;
