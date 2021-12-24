ef356345932736b4a80f65a42116191080b8be48
FoxWareFX <foxwarefx@gmail.com>
2021-12-24 19:38:41 +0200

    moved protected mode switch outside bootloader

--- asm/ext.asm
+++ asm/ext.asm
@@ -1,8 +1,35 @@
-%include "asm/gdt.asm"
+[bits 16]
 
+; DEFINES ;
+%include "asm/gdt.asm"
 extern start
 extern sys_kill
 
+; SWITCH TO PROTECTED MODE ;
+cli
+lgdt [GDT_desc]
+mov EAX, CR0
+or EAX, 1
+mov CR0, EAX
+
+; FAR JUMP ;
+jmp CODE_SEG:protMode
+
+; PROTECED MODE ;
 [bits 32]
+protMode:
+
+; SET DATA SEGEMENT ;
+mov AX, DATA_SEG
+mov DS, AX
+mov SS, AX
+mov ES, AX
+mov FS, AX
+mov GS, AX
+
+; JUMP TO C CODE ;
 call start
+
+; KILL IN CASE OF FAILURE ;
 call sys_kill
+jmp $
