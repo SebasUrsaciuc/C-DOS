28dd8519b313cb223e99241b6ecbd5c821c38126
FoxWareFX <foxwarefx@gmail.com>
2021-12-26 14:17:25 +0200

    now the sectors to load in memory are calculated

--- src/boot.s
+++ src/boot.s
@@ -1,14 +1,10 @@
 .code16
 .section .boot
 
-// DEFINES
-.equ PROGRAM_SPACE, 0x7E00
-.equ KERNEL_SECTORS, 36
-
 // LOAD THE KERNEL ONTO RAM
 mov $0x02, %Ah
-mov $KERNEL_SECTORS, %Al
-mov $PROGRAM_SPACE, %BX
+mov $progSects, %Al
+mov $progSpace, %BX
 mov $0x00, %Ch
 mov $0x02, %Cl
 mov $0x00, %Dh
